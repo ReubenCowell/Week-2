ac2c94deb9765da07257830993de9226cb488a93
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2021-08-31 14:15:09 +0100

    Added information to see how many pixels were lost
    gist: https://gist.github.com/ToastedToast9996/b923f8bdb1da154d2431e63fef64ff28

--- sending_images/imageviewer.py
+++ sending_images/imageviewer.py
@@ -1,21 +1,50 @@
 import socket
 import pickle
 from fl_networking_tools import ImageViewer
+from random import randint
 
-viewer = ImageViewer()
+viewer = ImageViewer()  # starts an imageviewer window
 
 
-def get_pixel_data():
+"""
+This simple error detection algorithm will work like this:
+
+Receive a pixel.
+Store the position of the pixel received.
+Receive a new pixel.
+Ask whether the position of the new pixel is more than 1 greater in x or y than the last pixel.
+If so, a pixel has been lost.
+"""
+
+def get_pixel_data():  # gets the pixel data
     while True:
-        data, client_address = udp_server.recvfrom(1024)
-        message = pickle.loads(data)
+        global lost_pixels
+        viewer.text = lost_pixels  # at the top of the image viwer, it will display the number of lost pizels
+        data, client_address = udp_server.recvfrom(1024)  
+        message = pickle.loads(data)  # 'decrypts the pickle data
+        # gets the position and rgba data for each pixel
         pos = message[0]
         rgba = message[1]
-        viewer.put_pixel(pos, rgba)
-        print(pos,rgba)
+        viewer.put_pixel(pos, rgba)  # puts the colour on the imageviewer window
+        print(pos,rgba)  # prints the data
+        print(last_pixel_updated)  
+        if (pos[0] - last_pixel_updated[0] > 1) or (pos[1] - last_pixel_updated[1] > 1): # checks if a pixel is out of sequence by seeing whether their locations subtract to more than one
+            lost_pixels += 1  # this is teh variable for the amount of lost pixels
+            viewer.text = lost_pixels  # updates the text at the top of the imageviewer window
 
+    #last_pixel_updated = pos
 
+# binds the socket as a datagram server
 udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_server.bind(("0.0.0.0", 20001))
 
-viewer.start(get_pixel_data)
+tot_pixels, client_address = udp_server.recvfrom(1024)
+print(tot_pixels.decode())
+
+# sets the variables:
+lost_pixels = 0     
+last_pixel_updated = (-1, -1)
+viewer.start(get_pixel_data)  # starts the image viewer with the get_pixel_data function
+
+recvd_pixels = int(tot_pixels.decode()) - lost_pixels
+print('Image recieved:\nTotal pixels: {}\nLost pixels: {}\nPixels recieved: {}'.format(tot_pixels,lost_pixels,recvd_pixels))
--- sending_images/main.py
+++ sending_images/main.py
@@ -2,19 +2,25 @@ from PIL import Image
 import pickle
 import socket
 import time
-udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+from random import randint 
+udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # the sets up as a client
 
-image = Image.open("image.bmp")
+# gets the image to send:
+image = Image.open("/Users/reubencowell/OneDrive - Joyce Frankland Academy/Coding/Python/Networking with python/Week-2/sending_images/image.bmp")
 
-width, height = image.size
-print(width, height)
+width, height = image.size  # gets the height of the image
+print(width, height)  # displays it to the user
+tot_pixels = width*height
+udp_client.sendto(str(tot_pixels).encode(), ("127.0.0.1", 20001))
+print(tot_pixels)
 
+# for every row, send every pixel's x and y coordinates to the server
 for y in range(height):
     for x in range(width):
         pos = (x, y)
-        rgba = image.getpixel(pos)
+        rgba = image.getpixel(pos)  # gets the pixel and sees its
         message = (pos,rgba)
         data = pickle.dumps(message)
-        udp_client.sendto(data, ("127.0.0.1", 20001))
+        udp_client.sendto(data, ("127.0.0.1", 20001))  # sends the data (the position and rgba value)
         time.sleep(0.001)
-
+print('sent image')  
